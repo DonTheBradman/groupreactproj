b738729a568b815874392bc85cc84e3ed8149505
DonTheBradman <bphipps147@gmail.com>
2021-09-17 15:40:22 +0100

    it works, fully complete project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import Component from "react";
+// define some functional components
+const AlbumTitle = (props) => <h2 class="album-title">{props.title}</h2>;
+const AlbumArtist = (props) => <h3 class="album-artist">{props.name}</h3>;
+const AlbumTracks = (props) => {
+  return props.visible ? (
+    <ol class="album-tracks">
+      {props.tracks.map((track, i) => (
+        <li key={i}>{track}</li>
+      ))}
+    </ol>
+  ) : null;
+};
+const ShowHideButton = (props) => (
+  <button onClick={props.toggle}>{props.visible ? "Hide" : "Show"}</button>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// the album will be maintaining some state, so make this a class component
+class Album extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { visible: false };
+    this.toggle = this.toggle.bind(this);
+  }
+  toggle() {
+    this.setState({ visible: this.state.visible ? false : true });
+  }
+  render() {
+    return (
+      <section
+        id={"album-" + this.props.title.toLowerCase().replace(/ /g, "-")}
+      >
+        <AlbumTitle title={this.props.title} />
+        <AlbumArtist name={this.props.artist} />
+        <ShowHideButton visible={this.state.visible} toggle={this.toggle} />
+        <AlbumTracks tracks={this.props.tracks} visible={this.state.visible} />
+      </section>
+    );
+  }
+}
+
+// another functional component for the top level element
+class App extends React.Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {music: []};
+  }
+
+  componentDidMount() {
+    axios.get("/music.json").then(response => {
+      //console.log(response.data);
+      this.setState({music: response.data});
+      //console.log(response.data);
+    })
+    .catch((e) => {
+      console.log("Error occurred " + e);
+    });
+  }
+
+  render() {
+    return (
+    <article>
+      <h1>Record Collection</h1>
+      <div className="albums">
+        {this.state.music.map((album, i) => (
+          <Album
+            title={album.title}
+            artist={album.artist}
+            tracks={album.tracks}
+          />
+        ))}
+      </div>
+    </article>
+  )};
+};
+
+ReactDOM.render(<App/>, document.getElementById("root"));
